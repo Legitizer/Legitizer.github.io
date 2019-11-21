61ea8d6f2d2e9fc29ec31cafd50ca92ba29099c1
Legitizer <32262350+Legitizer@users.noreply.github.com>
2019-11-21 16:17:52 +0100

    Add files via upload

---
+++ js/items.js
@@ -0,0 +1,280 @@
+//STARTER ITEMS
+let starters = [
+    {url: "https://web2.hirez.com/smite/item-icons/mages-blessing.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/assassins-blessing.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/hunters-blessing.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/guardians-blessing.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/warriors-blessing.jpg"}
+]
+
+//OFFENSE PHYSICALS
+let physical_core = [
+    {url: "https://web2.hirez.com/smite/item-icons/warrior-tabi.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/ninja-tabi.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/reinforced-greaves.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/talaria-boots.jpg"}
+]
+
+let physical_OFFENSE = [
+    {url: "https://web2.hirez.com/smite/item-icons/the-executioner.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/qins-sais.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/titans-bane.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/brawlers-beat-stick.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/jotunns-wrath.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/the-crusher.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/transcendence.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/hydras-lament.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/deathbringer.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/rage.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/malice.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/soul-eater.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/devourers-gauntlet.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/bloodforge.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/frostbound-hammer.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/runeforged-hammer.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/blackthorn-hammer.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/silverbranch-bow.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/atalanatas-bow.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/wind-demon.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/poisoned-star.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/bumbas-mask.jpg"}
+]
+
+let assassins_OFFENSE = [
+    {url: "https://web2.hirez.com/smite/item-icons/masamune.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/hastened-katana.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/golden-blade.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/stone-cutting-sword.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/arondight.jpg"}
+]
+
+let hunters_OFFENSE = [
+    {url: "https://web2.hirez.com/smite/item-icons/fail-not.jpg"}
+]
+
+let warriors_OFFENSE = [
+    {url: "https://web2.hirez.com/smite/item-icons/rangdas-mask.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/arondight.jpg"}
+]
+
+//OFFENSE MAGICALS
+let magical_core = [
+    {url: "https://web2.hirez.com/smite/item-icons/shoes-of-the-magi.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/shoes-of-focus.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/reinforced-shoes.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/travelers-shoes.jpg"}
+]
+
+let magical_OFFENSE = [
+    {url: "https://web2.hirez.com/smite/item-icons/pythagorems-piece.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/bancrofts-talon.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/typhons-fang.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/soul-gem.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/demonic-grip.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/telkhines-ring.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/shamans-ring.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/hastened-ring.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/obsidian-shard.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/divine-ruin.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/spear-of-the-magus.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/spear-of-desolation.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/gem-of-isolation.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/warlocks-staff.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/ethereal-staff.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/rod-of-asclepius.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/book-of-thoth.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/polynomicon.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/soul-reaver.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/rod-of-tahuti.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/chronos-pendant.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/staff-of-myrddin.jpg"},
+]
+
+let mages_OFFENSE = [
+    
+]
+
+let guardians_OFFENSE = [
+    
+]
+
+//DEFENSE
+let DEFENSE = [
+    {url: "https://web2.hirez.com/smite/item-icons/sovereignty.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/mystical-mail.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/midgardian-mail.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/hide-of-the-nemean-lion.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/breastplate-of-valor.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/spectral-armor.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/contagion.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/magis-cloak.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/hide-of-the-urchin.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/spirit-robe.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/mantle-of-discord.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/bulwark-of-hope.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/pestilence.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/heartward-amulet.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/talisman-of-energy.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/genjis-guard.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/shoguns-kusari.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/pridwen.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/witchblade.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/winged-blade.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/relic-dagger.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/toxic-blade.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/stone-of-gaia.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/shield-of-regrowth.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/mail-of-renewal.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/gauntlet-of-thebes.jpg"},
+
+]
+
+let assassins_DEFENSE = [
+    {url: "https://web2.hirez.com/smite/item-icons/runic-shield.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/ancile.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/emperors-armor.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/shifters-shield.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/void-shield.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/lonos-mask.jpg"},
+]
+
+let hunters_DEFENSE = [
+    {url: "https://web2.hirez.com/smite/item-icons/runic-shield.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/ancile.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/emperors-armor.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/shifters-shield.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/void-shield.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/lonos-mask.jpg"}
+]
+
+let warriors_DEFENSE = [
+    {url: "https://web2.hirez.com/smite/item-icons/runic-shield.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/ancile.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/emperors-armor.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/shifters-shield.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/void-shield.jpg"}
+]
+
+let guardians_DEFENSE = [
+    {url: "https://web2.hirez.com/smite/item-icons/void-stone.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/stone-of-fal.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/celestial-legion-helm.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/lotus-crown.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/jade-emperors-crown.jpg"}
+]
+
+let mages_DEFENSE = [
+    {url: "https://web2.hirez.com/smite/item-icons/void-stone.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/stone-of-fal.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/celestial-legion-helm.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/lotus-crown.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/jade-emperors-crown.jpg"},
+    {url: "https://web2.hirez.com/smite/item-icons/lonos-mask.jpg"}
+]
+
+
+let itemStarter = document.getElementById("itemStarter");
+let item1 = document.getElementById("item1");
+let item2 = document.getElementById("item2");
+let item3 = document.getElementById("item3");
+let item4 = document.getElementById("item4");
+let item5 = document.getElementById("item5");
+let item6 = document.getElementById("item6");
+
+let mageR = document.getElementById("mageR");
+let assassinR = document.getElementById("assassinR");
+let hunterR = document.getElementById("hunterR");
+let warriorR = document.getElementById("warriorR");
+let guardianR = document.getElementById("guardianR");
+
+let offenseCB = document.getElementById("offenseCB");
+let defenseCB = document.getElementById("defenseCB");
+
+
+function rollBuild(){
+    let sItem = Math.round(Math.random()*(starters.length-1));
+    let cItem = Math.round(Math.random()*(4-1));
+    let newList = [];
+
+    if (offenseCB.checked == false && defenseCB.checked == false){
+        offenseCB.checked = true
+        defenseCB.checked = true
+    }
+
+    if (mageR.checked == true){
+        item1.src = magical_core[cItem]['url']
+        if (offenseCB.checked == true){
+            newList = newList.concat(magical_OFFENSE);
+            newList = newList.concat(mages_OFFENSE);
+        }
+        if (defenseCB.checked == true){
+            newList = newList.concat(DEFENSE);
+            newList = newList.concat(mages_DEFENSE);
+        }
+    }else if (assassinR.checked == true){
+        item1.src = physical_core[cItem]['url']
+        if (offenseCB.checked == true){
+            newList = newList.concat(physical_OFFENSE);
+            newList = newList.concat(assassins_OFFENSE);
+        }
+        if (defenseCB.checked == true){
+            newList = newList.concat(DEFENSE);
+            newList = newList.concat(assassins_DEFENSE);
+        }
+    }
+    else if (hunterR.checked == true){
+        item1.src = physical_core[cItem]['url']
+        if (offenseCB.checked == true){
+            newList = newList.concat(physical_OFFENSE);
+            newList = newList.concat(hunters_OFFENSE);
+        }
+        if (defenseCB.checked == true){
+            newList = newList.concat(DEFENSE);
+            newList = newList.concat(hunters_DEFENSE);
+        }
+    }
+    else if (warriorR.checked == true){
+        item1.src = physical_core[cItem]['url']
+        if (offenseCB.checked == true){
+            newList = newList.concat(physical_OFFENSE);
+            newList = newList.concat(warriors_OFFENSE);
+        }
+        if (defenseCB.checked == true){
+            newList = newList.concat(DEFENSE);
+            newList = newList.concat(warriors_DEFENSE);
+        }
+    }
+    else if (guardianR.checked == true){
+        item1.src = magical_core[cItem]['url']
+        if (offenseCB.checked == true){
+            newList = newList.concat(magical_OFFENSE);
+            newList = newList.concat(guardians_OFFENSE);
+        }
+        if (defenseCB.checked == true){
+            newList = newList.concat(DEFENSE);
+            newList = newList.concat(guardians_DEFENSE);
+        }
+    }
+
+    itemStarter.src = starters[sItem]['url']
+
+    let r2 = Math.round(Math.random()*(newList.length-1));
+    item2.src = newList[r2]['url']
+    newList.splice(r2, 1)
+
+    let r3 = Math.round(Math.random()*(newList.length-1));
+    item3.src = newList[r3]['url']
+    newList.splice(r3, 1)
+
+    let r4 = Math.round(Math.random()*(newList.length-1));
+    item4.src = newList[r4]['url']
+    newList.splice(r4, 1)
+
+    let r5 = Math.round(Math.random()*(newList.length-1));
+    item5.src = newList[r5]['url']
+    newList.splice(r5, 1)
+
+    let r6 = Math.round(Math.random()*(newList.length-1));
+    item6.src = newList[r6]['url']
+    newList.splice(r6, 1)
+}
